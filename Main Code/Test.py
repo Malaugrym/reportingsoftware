83d7b8baab7fb8ff7159baf86f72f674ebf892e5
bmparks5150 <bmparks5150@users.noreply.github.com>
2018-01-17 09:47:44 -0500

    Basic Auth

--- Main Code/Test.py
+++ Main Code/Test.py
@@ -1,3 +1,13 @@
-import urllib2
+import json
+import urllib
+import urllib.request as urllib2
+import requests
 
-test_api = 'test'
+url = 'https://summithosting.kayako.com/api/v1/users'
+
+username = 'bparks@summithosting.com'
+pw = 'Welcome1!'
+
+req = requests.get(url, auth=requests.auth.HTTPBasicAuth(username,pw))
+
+print (json.loads(req.text))
